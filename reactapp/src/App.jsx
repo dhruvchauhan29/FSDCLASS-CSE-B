1707f38719f7f21bcccaf85179ace69c9532b129
dhruvchauhan29 <dhruvpratapsingh29@gmail.com>
2024-11-14 12:27:07 +0530

    commmitted

--- reactapp/src/App.jsx
+++ reactapp/src/App.jsx
@@ -1,15 +1,48 @@
 import React from 'react'
 import Student from './Student';
+import './App.css'
 function App() {
   const h1=<h1>hello</h1>;
   const color={
     backgroundColor:'red',
     color:'yellow'
   }
+  const data=[
+    {
+      name:"Rahul",
+      branch:"CSE",
+      college:"ABES",
+      rollno:"20",
+      sec:"B",
+    }
+    ,{
+      name:"Rahul",
+      branch:"CSE",
+      college:"ABES",
+      rollno:"20",
+      sec:"B",
+    }
+    ,{
+      name:"Rahul",
+      branch:"CSE",
+      college:"ABES",
+      rollno:"20",
+      sec:"B",
+    }
+  ]
   return (
     <div>
         <div style={color}>{h1}</div>
-        <div>
+        <div className='st'>
+           <Student data={data}></Student>
+           {/* <Student 
+            name="Dhruv" 
+            pic={<img src='https://commondatastorage.googleapis.com/codeskulptor-demos/riceracer_assets/img/srixner.png'></img>}
+            branch="CSE" 
+            rollno="20" 
+            college="ABES Engineering college"
+            sec="B"
+           ></Student>
            <Student 
             name="Dhruv" 
             pic={<img src='https://commondatastorage.googleapis.com/codeskulptor-demos/riceracer_assets/img/srixner.png'></img>}
@@ -18,6 +51,14 @@ function App() {
             college="ABES Engineering college"
             sec="B"
            ></Student>
+           <Student 
+            name="Dhruv" 
+            pic={<img src='https://commondatastorage.googleapis.com/codeskulptor-demos/riceracer_assets/img/srixner.png'></img>}
+            branch="CSE" 
+            rollno="20" 
+            college="ABES Engineering college"
+            sec="B"
+           ></Student> */}
         </div>
     </div>
   )
--- reactapp/src/Student.jsx
+++ reactapp/src/Student.jsx
@@ -1,19 +1,21 @@
 import React from 'react';
 import './student.css';
 
-function Student(props) {
+function Student({data}) {
+  
   return (
     <div className='icard'>
-        <table>
+      {JSON.stringify(data)}
+        {/* <table>
             <tbody>
                 <tr><td colSpan={2}>{props.college}</td></tr>
-                <tr><td>{props.pic}</td></tr>
+                <tr><td style={{widows}}>{props.pic}</td></tr>
                 <tr><td>Name</td><td>{props.name}</td></tr>
                 <tr><td>Branch</td><td>{props.branch}</td></tr>
                 <tr><td>sec</td><td>{props.sec}</td></tr>
                 <tr><td>Rollno</td><td>{props.rollno}</td></tr>
             </tbody>
-        </table>
+        </table> */}
     </div>
   )
 }
